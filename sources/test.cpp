9be82045f0a6840ba45f456f99d0ae13352e7557
vbrodskiy <vadim.brodskiy@centrale-marseille.fr>
2012-12-17 16:43:27 +0100

    added some description

--- sources/test.cpp
+++ sources/test.cpp
@@ -1,3 +1,10 @@
+/*! \brief Collaboration test
+ *		with documentation
+ *
+ * Attempt to introduce collaboration work at classes of
+ * Infirmatique industrielle
+ */
+
 #include<isotream>
 using namespace std;
 int main(int argc, char ** argv)
